7c617acb69b8b5270160118d8930749f143e5ef4
David Weston <david.weston@sapient.com>
2021-02-17 12:20:57 +0000

    fix

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,10 +5,10 @@ import logo from "../assets/logo.svg";
 const Hero = () => (
   <div className="text-center hero my-5">
     <img className="mb-3 app-logo" src={logo} alt="React logo" width="120" />
-    <h1 className="mb-4">Travel0</h1>
+    <h1 className="mb-4">Cruise0 by Travel0</h1>
 
     <p className="lead">
-      Welcome to Travel0
+      Welcome to Cruise0 Demo Application
     </p>
   </div>
 );
